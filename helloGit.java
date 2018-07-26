a4da00d17434b5e32a025afcf297d50ce49e404d
shubham kadam <shubham_kadam@persistent.co.in>
2018-07-26 15:38:47 +0530

    new Date() added

--- helloGit.java
+++ helloGit.java
@@ -1,12 +1,12 @@
 class hello{
 
 static void display(){
-	system.out.println("Display Todays date");
+	system.out.println("Display Todays date"+new Date());
 }
 
 public static void main(String args[]){
 
-	System.out.println("hello...welcome to GIT ");
+	System.out.println("hello...!!!welcome to GIT!!! ");
 	display();
 
 }
