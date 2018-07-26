59d3b869f562782aac3a50bb9efc6daba9e30952
shubham kadam <shubham_kadam@persistent.co.in>
2018-07-26 15:08:45 +0530

    calling static display()

--- helloGit.java
+++ helloGit.java
@@ -1,12 +1,12 @@
 class hello{
 
-void display(){
+static void display(){
 	system.out.println("Display Todays date");
 }
 
 public static void main(String args[]){
-	System.out.println("hello...welcome to GIT
- ");
+	System.out.println("hello...welcome to GIT ");
+	display();
 }
 
 }
